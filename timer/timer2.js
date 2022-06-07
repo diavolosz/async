c88f921c29609e05151ae59027cec56843c5de73
Le Duong Hien Thong <michaeldiavolos@gmail.com>
2022-06-07 22:12:53 +0000

    added timer 2'

---
+++ timer/timer2.js
@@ -0,0 +1,14 @@
+process.stdin.setRawMode(true)
+process.stdin.setEncoding('utf8')
+
+process.stdin.on('data', (key) => {
+  if (key === "b") {
+    process.stdout.write("\x07" + "BEEP! \n")
+  } else if (key === "\u0003") {
+    process.exit()
+  } else {
+    setTimeout(() => {
+      process.stdout.write("\x07" + "BEEP! \n");
+    }, key * 1000)
+  }
+})
